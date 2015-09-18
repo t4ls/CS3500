f056e5000212aaba8c56fdb45d5f96fbdf461ac3
Blake Wrege <blakewrege1@gmail.com>
2015-09-18 16:37:38 -0400

    worked on stuff

--- Assignment-1/index.php
+++ Assignment-1/index.php
@@ -1,5 +1,5 @@
-<?php require_once '../vendor/autoload.php'; ?>
-<?php $loader = new Twig_Loader_Filesystem('../views/'); ?>
+<?php require_once '../twig/vendor/autoload.php'; ?>
+<?php $loader = new Twig_Loader_Filesystem('../twig/views/'); ?>
 
 <?php $twig = new Twig_Environment($loader); ?>
 <?php echo $twig->render('base.html', array('header' => 'Assignment 1','path' => 'https://cs.wmich.edu/~brp9209/CS3500/')); ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php require_once './vendor/autoload.php'; ?>
-<?php $loader = new Twig_Loader_Filesystem('./views/'); ?>
+<?php require_once './twig/vendor/autoload.php'; ?>
+<?php $loader = new Twig_Loader_Filesystem('./twig/views/'); ?>
 
 <?php $twig = new Twig_Environment($loader); ?>
 
