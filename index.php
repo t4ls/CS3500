fa8a1d3e57900e82242f9ef138c476c7388f068f
gigglesbw4 <blakewrege1@gmail.com>
2015-09-18 17:21:23 -0400

    fixed the linking

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 
 <?php $twig = new Twig_Environment($loader); ?>
 
-<?php echo $twig->render('base.html', array('header' => 'CS 3500','path' => 'https://cs.wmich.edu/~brp9209/CS3500/')); ?>
+<?php echo $twig->render('base.html', array('header' => 'CS 3500')); ?>
 
 <h2> Work in progress </h2>
