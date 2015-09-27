7b5228343641726073809529b011866518fbb16e
GoldenBoy <amolsch.patrick@gmail.com>
2015-09-27 16:28:46 -0400

    added some videos and stuff

--- Assignment-1/index.php
+++ Assignment-1/index.php
@@ -4,8 +4,16 @@
 <?php $twig = new Twig_Environment($loader); ?>
 <?php echo $twig->render('base.html', array('header' => 'Assignment 1')); ?>
 <div class="container">
-<div class="one-half column" style="margin-top: 5%">
-<h2> Hello World </h2>
-<p> Patrick write stuff here. And then Patrick said, "Yes, Commander Blake. I shall."</p>
+<br>
+<h2 align="center"><b> Hello World: Welcome to HTML5 and CSS3 </b></h2>
+<br>
+<h2 style="margin-left:4%"><b><u>Videos!</u></b></h2>
+<div class="one-half column" style="margin-top: 1%">
+<h3><b> HTML5 Tutorial by Derek Banas</b></h3>
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/kDyJN7qQETA" frameborder="0" allowfullscreen></iframe>
+<p>A little lengthy, but it provides an excellent range of fundamental functionalities of HTML5. Enjoy, nerds :D </p>
+<h3><b> CSS Tutorial by Derek Banas</b></h3>
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/CUxH_rWSI1k" frameborder="0" allowfullscreen></iframe>
+<p> And then Patrick said, "Yes, Commander Blake--I shall learn CSS3 and be the best I ever could."</p>
 </div>
 </div>
