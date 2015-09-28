66882d0838927ce86799361706dbb1e6e1450e10
gigglesbw4 <blakewrege1@gmail.com>
2015-09-28 07:18:50 -0400

    gooldenboy++

--- Assignment-1/index.php
+++ Assignment-1/index.php
@@ -3,7 +3,10 @@
 
 <?php $twig = new Twig_Environment($loader); ?>
 <?php echo $twig->render('base.html', array('header' => 'Assignment 1')); ?>
+<html>
+<body>
 <div class="container">
+<div class="row">
 <br>
 <h2 align="center"><b> Hello World: Welcome to HTML5 and CSS3 </b></h2>
 <br>
@@ -11,9 +14,29 @@
 <div class="one-half column" style="margin-top: 1%">
 <h3><b> HTML5 Tutorial by Derek Banas</b></h3>
 <iframe width="100%" height="315" src="https://www.youtube.com/embed/kDyJN7qQETA" frameborder="0" allowfullscreen></iframe>
-<p>A little lengthy, but it provides an excellent range of fundamental functionalities of HTML5. Enjoy, nerds :D </p>
+<p>A little lengthy, but it provides an excellent range of fundamental functionalities of HTML5. </p>
 <h3><b> CSS Tutorial by Derek Banas</b></h3>
 <iframe width="100%" height="315" src="https://www.youtube.com/embed/CUxH_rWSI1k" frameborder="0" allowfullscreen></iframe>
-<p> And then Patrick said, "Yes, Commander Blake--I shall learn CSS3 and be the best I ever could."</p>
 </div>
 </div>
+  <div class="row">
+<h2 align="center"><b>Tools Used</b></h2>
+  </div>
+  <div class="row">
+    <div class="one-half column" style "margin-top: 5000px;">
+<p style "  padding-top: 25px;"> Skeleton is light weight responsive framework that can be used to make sites look cool on mobile as well as desktops without having to reinvent the wheel. </p>
+    </div>
+    <div class="one-half column">
+        	<img src="./twig/images/skeleton.png"> 
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+</div>
+</body>
+</html>
