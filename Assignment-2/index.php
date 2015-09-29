c04f1c2f5d6258715f7bc2deee2d357b168e20ad
GoldenBoy <amolsch.patrick@gmail.com>
2015-09-29 11:28:42 -0400

    created assignment two on menu bar, i hope

---
+++ Assignment-2/index.php
@@ -0,0 +1,18 @@
+<?php require_once '../twig/vendor/autoload.php'; ?>
+<?php $loader = new Twig_Loader_Filesystem('../twig/views/'); ?>
+
+<?php $twig = new Twig_Environment($loader); ?>
+<?php echo $twig->render('base.html', array('header' => 'Assignment 2')); ?>
+
+<html>
+
+<body>
+
+
+
+
+
+
+</body>
+
+</html>
