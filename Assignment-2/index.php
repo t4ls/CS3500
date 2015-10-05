e42820acf80d14460b6953f0e426abc72705de84
gigglesbw4 <blakewrege1@gmail.com>
2015-10-05 15:58:03 -0400

    started A2, patrick add stuff

--- Assignment-2/index.php
+++ Assignment-2/index.php
@@ -7,13 +7,40 @@
 <html>
 
 <body>
-	<div class="container">
-		<div class="row">
+    <div class="container">
+        <div class="row">
             <br>
             <h2 align="center"><b> Welcome to Javascript </b></h2>
+            <br>
+            <div class="row" style="text-align:center;">
+                <h4><b> Javascript Tutorial by Derek Banas</b></h4>
+                <iframe width="75%" height="315" src="https://www.youtube.com/embed/fju9ii8YsGs" frameborder="0" allowfullscreen></iframe>
+                <p>Add Javascript tutorial stuff here</p>
+            </div>
+        </div>
+        <div class="row" style="padding-top:2%;border-top: 1px solid ">
+            <h2 align="center"><b>Javascript Standards</b></h2>
+            <p>For these standards we used <a href="http://javascript.crockford.com/code.html">Crockford's JavaScript</a> coding guidelines </p>
+            <ul>
+                <li> Javascript programs are stored in and delivered as .js file type.</li>
+                <li> JavaScript code should not be embedded in HTML files unless the code is specific to a single session.</li>
+            </ul>
 
-	</div>
-	</div>
+        </div>
+        <div class="row" style="padding-top:2%;border-top: 1px solid ">
+            <h2 align="center"><b>Javascript Examples</b></h2>
+            <p>Add Javascript examples stuff here</p>
+            <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">
+                Click me to display Date and Time.</button>
+
+            <p id="demo"></p>
+        </div>
+
+        <div class="row" style="padding-top:2%;border-top: 1px solid ">
+            <h2 align="center"><b>Javascript Frameworks</b></h2>
+        </div>
+    </div>
+
 
 </body>
 
