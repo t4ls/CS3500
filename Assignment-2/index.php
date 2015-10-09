4117bfe26f054a424b30a21d5f78d4200bd5de59
blake <Blakewrege1@gmail.com>
2015-10-09 12:39:04 -0400

    made A2 html pretty

--- Assignment-2/index.php
+++ Assignment-2/index.php
@@ -5,12 +5,12 @@
 <?php echo $twig->render('base.html', array('header' => 'Assignment 2')); ?>
 
 <?php require '../Assignment-2/newuser.php';?>
-<html>
 
-<script src="//code.jquery.com/jquery-1.10.2.min.js"></script> 
+
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="//www.opencpu.org/js/archive/opencpu-0.4.js"></script>
 
-
+<html>
 <body>
     <div class="container">
         <div class="row">
@@ -25,11 +25,13 @@
         </div>
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Javascript Standards</b></h2>
-            <p>For these standards we used <a href="http://javascript.crockford.com/code.html">Crockford's JavaScript</a> coding guidelines </p>
+            <p>For these standards we used <a href="http://javascript.crockford.com/code.html">Crockford's JavaScript</a> coding
+                guidelines </p>
             <ul>
                 <li> Javascript programs are stored in and delivered as .js file type.</li>
                 <li> JavaScript code should not be embedded in HTML files unless the code is specific to a single session.</li>
-                <li> &lt;script src=filename.js &gt; tags should be placed as far down in the body as possible to reduce the delays caused by loading the script</li>
+                <li> &lt;script src=filename.js &gt; tags should be placed as far down in the body as possible to reduce the delays
+                    caused by loading the script</li>
             </ul>
 
         </div>
@@ -37,63 +39,71 @@
             <h2 align="center"><b>Javascript Examples</b></h2>
             <p>Add Javascript examples stuff here</p>
             <button type="button-primary" onclick="document.getElementById('demo').innerHTML = Date()">
-            Click to display Date and Time.</button>
+                Click to display Date and Time.</button>
             <p id="demo"></p>
-	    <div class="row">
-	    <div class="one-half column" > 
-            <form name="newuser" form action="" onsubmit="return validateNew();" method="post" >
-            <div class="form_row"><span class="form_label">Username: </span><input type="text" name="username"></div>
-            <div class="form_row"><input type="submit" value="Submit" name="submitbutton"></div>
-            </form>
-	    </div>
-	    <div class="one-half column" >
-	    <p>Displays the last 10 users: <?php echo json_encode($arr); ?></p>
+            <div class="row">
+                <div class="one-half column">
+                    <form name="newuser" form action="" onsubmit="return validateNew();" method="post">
+                        <div class="form_row"><span class="form_label">Username: </span>
+                            <input type="text" name="username">
+                        </div>
+                        <div class="form_row">
+                            <input type="submit" value="Submit" name="submitbutton">
+                        </div>
+                    </form>
+                </div>
+                <div class="one-half column">
+                    <p>Displays the last 10 users:
+                        <?php echo json_encode($arr); ?>
+                    </p>
+                </div>
             </div>
-	</div>
-	</div>
+        </div>
 
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Javascript Frameworks</b></h2>
-	    <h4 aligin=><b>jQuery JavaScript Library</b></h4>
-            <p>One framework we are planning to use is jQuery. jQuery is a fast, small, and feature-rich JavaScript library. It is designed to make client
-	    side scripting easier for HTML. This button is hidden when clicked on because of a jQuery script <hidden><button>Click Me</button></hidden></p>
-	 <h4 aligin=><b>OpenCPU Framework</b></h4>
-	<p>OpenCPU is a framework for embedded scientific computing and reproducible research. We are using it here to run R code. OpenCPU requires jQuery to
-	provide Ajax wrappers for calling R from within a web page.</p>
-	<textarea id="input" rows="10" cols="50" style="width:70%; height: 120px; display: block; margin-left: auto; margin-right: auto;">
-	function(){
-	   hello <- 1:10
-  	return(hello+1)   
-	}
-	</textarea> 
-	<br /> <button id="submitbutton" type="button">Call R</button>
+            <h4 aligin=><b>jQuery JavaScript Library</b></h4>
+            <p>One framework we are planning to use is jQuery. jQuery is a fast, small, and feature-rich JavaScript library.
+                It is designed to make client side scripting easier for HTML. This button is hidden when clicked on because
+                of a jQuery script
+                <hidden>
+                    <button>Click Me</button>
+                </hidden>
+            </p>
+            <h4 aligin=><b>OpenCPU Framework</b></h4>
+            <p>OpenCPU is a framework for embedded scientific computing and reproducible research. We are using it here to run
+                R code. OpenCPU requires jQuery to provide Ajax wrappers for calling R from within a web page.</p>
+            <textarea id="input" rows="10" cols="50" style="width:70%; height: 120px; display: block; margin-left: auto; margin-right: auto;">
+                function(){ hello = 1:10 return(hello+1) }
+            </textarea>
+            <br />
+            <button id="submitbutton" type="button">Call R</button>
 
-<pre><code id="output"></code></pre>
-	
-	</div>
+            <pre><code id="output"></code></pre>
+
+        </div>
 
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Compatability</b></h2>
-	<p>ADD CONTENT</p>
-	</div>
+            <p>ADD CONTENT</p>
+        </div>
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Security</b></h2>
-	<p>ADD CONTENT</p>
-	</div>
+            <p>ADD CONTENT</p>
+        </div>
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Tools and Debugging</b></h2>
-	<p>ADD CONTENT</p>
-	</div>    
-   </div>
+            <p>ADD CONTENT</p>
+        </div>
+    </div>
 
 
-<script>
-var users = <?php echo json_encode($arr); ?>;
-</script>
-<script src="./Assignment-2/Validator.js"></script>
-<script src="./Assignment-2/jqueryscripts.js"></script>
+    <script>
+        var users = <? php echo json_encode($arr); ?> ;
+    </script>
+    <script src="./Assignment-2/Validator.js"></script>
+    <script src="./Assignment-2/jqueryscripts.js"></script>
 
 </body>
-
 </html>
 
