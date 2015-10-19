5a95810b7535c19bd1b72e1cd29f2d5266ab9e72
GoldenBoy <amolsch.patrick@gmail.com>
2015-10-18 21:35:43 -0400

    added content. check it out.

--- Assignment-2/index.php
+++ Assignment-2/index.php
@@ -1,4 +1,4 @@
-<?php require_once '../twig/vendor/autoload.php'; ?>
+4<?php require_once '../twig/vendor/autoload.php'; ?>
 <?php $loader = new Twig_Loader_Filesystem('../twig/views/'); ?>
 
 <?php $twig = new Twig_Environment($loader); ?>
@@ -21,9 +21,33 @@
             <div class="row" style="text-align:center;">
                 <h4><b> Javascript Tutorial by Derek Banas</b></h4>
                 <iframe width="90%" height="315" src="https://www.youtube.com/embed/fju9ii8YsGs" frameborder="0" allowfullscreen></iframe>
-                <p>Add Javascript tutorial stuff here</p>
+                <p>Derek Banas has many other instuctional videos on his YouTube channel. If you have further interest in these subjects, check it out!</p>
             </div>
         </div>
+        <div class="row" style="text-align:center;">
+        <p>Another fantastic source for becoming comfortable with writing your own Javascript is <a href="http://www.w3schools.com/js/default.asp" style="color:blue;">w3schools</a>. The
+        following are simple examples that you can find on w3schools that involve playing around with some Javascript.</p>
+        </div>
+        <div class="row" style="margin-top: 1%;display: block;margin-left: auto;margin-right: auto;text-align:center;">
+
+                <div class="one-half column" style="text-align:center;">
+                <p id="change1">JavaScript can change the style of an HTML element.</p>
+					<script>
+						function myFunction() {
+    					var x = document.getElementById("change1");
+   						 x.style.fontSize = "25px";           
+   						 x.style.color = "purple"; 
+						}
+					</script>
+				<button type="button" onclick="myFunction()">Click Me!</button>
+                </div>
+                <div class="one-half column" style="text-align:center;">
+                    <p id="change2">JavaScript can change HTML content, too!</p>
+					<button type="button" onclick="document.getElementById('change2').innerHTML = 'Hello, changed content!'">
+					Click Me!</button>
+                </div>
+        </div>
+
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Javascript Standards</b></h2>
             <p>For these standards we used <a href="http://javascript.crockford.com/code.html">Crockford's JavaScript</a> coding
@@ -37,7 +61,7 @@
 
         </div>
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
-            <h2 align="center"><b>Javascript Examples</b></h2>
+            <h2 align="center"><b>More Javascript Examples</b></h2>
             <p>This is a simple example of Javascript used to send the date to html id "demo"</p>
             <button type="button-primary" onclick="document.getElementById('demo').innerHTML = Date()">
                 Click to display Date and Time.</button>
@@ -98,10 +122,24 @@
         </div>
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Security</b></h2>
-            <p>ADD CONTENT</p>
+            <p>Since its release, there have been several JavaScript security issues that have gained widespread attention. 
+            For one, the way JavaScript interacts with the DOM poses a risk for end users by enabling malicious actors to 
+            deliver scripts over the web and run them on client computers. There are two measures that can be taken to contain 
+            this JavaScript security risk. First is sandboxing, or running scripts separately so that they can only access 
+            certain resources and perform specific tasks. The second measure is implementing the same origin policy, which 
+            prevents scripts from one site from accessing data that is used by scripts from other sites. Many JavaScript 
+            security vulnerabilities are the result of browser authors failing to take these measures to contain DOM-based 
+            JavaScript security risks.</p>
+            
+            <p style="color:gray;padding-left:25px">Excerpt from article written by Fergal Glynn</p>
         </div>
         <div class="row" style="padding-top:2%;border-top: 1px solid ">
             <h2 align="center"><b>Tools and Debugging</b></h2>
+            <p>Javascript has it's own debugger built into it. It will become very useful to programmers just getting started
+            with their Javascript adventures. It gives you the ability to implement breakpoints and other helpful features
+            when it comes to debugging your code. Many popular browsers have tools that involve debugging scripts that you have
+            written up, as well. For more information on the Javascript Debugger, visit this page 
+            <a href="http://www.w3schools.com/js/js_debugging.asp" style="color:blue;">here</a>.</p>
 
         </div>
     </div>
