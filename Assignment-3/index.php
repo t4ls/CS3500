77b86b077115a2ce9f945d09294ab110fc8cf44d
gigglesbw4 <blakewrege1@gmail.com>
2015-10-23 18:51:45 -0400

    added a3

--- Assignment-3/index.php
+++ Assignment-3/index.php
@@ -1,6 +1,5 @@
-
-<?php require_once './twig/vendor/autoload.php'; ?>
-<?php $loader = new Twig_Loader_Filesystem('./twig/views/'); ?>
+<?php require_once '../twig/vendor/autoload.php'; ?>
+<?php $loader = new Twig_Loader_Filesystem('../twig/views/'); ?>
 
 <?php $twig = new Twig_Environment($loader); ?>
 
