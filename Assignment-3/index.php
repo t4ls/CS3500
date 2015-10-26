1fd66a634aa859376fb907fe7d7c033548f9666e
gigglesbw4 <blakewrege1@gmail.com>
2015-10-26 15:22:31 -0400

    added webcam

--- Assignment-3/index.php
+++ Assignment-3/index.php
@@ -60,6 +60,29 @@ function videoError(e) {
 } 
 </script>
 
+
+<h1>Camera API</h1>
+
+            <section class="main-content">
+                <p>A demo of the Camera API, currently implemented in Firefox and Google Chrome on Android. Choose to take a picture with your device's camera and a preview will be shown through createObjectURL or a FileReader object (choosing local files supported too).</p>
+                
+                <p>
+                    <input type="file" id="take-picture" accept="image/*">
+                </p>
+
+                <h2>Preview:</h2>
+                <p>
+                    <img src="about:blank" alt="" id="show-picture">
+                </p>
+
+                <p id="error"></p>
+
+            </section>
+      <script src="./Assignment-3/base.js"></script>
+
+
+
+
 <h4> Added some cool R stuff</h4>
 <p>Run Arbitrary Function</p>
 <textarea id="input" rows="10" cols="50" style="width:50%; height: 120px;">
