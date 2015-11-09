b285af7e1819a0eaabd6aea5c7ba4629aac151cc
gigglesbw4 <blakewrege1@gmail.com>
2015-11-09 15:59:45 -0500

    added some color detection

--- Assignment-3/index.php
+++ Assignment-3/index.php
@@ -3,7 +3,7 @@
 
 <?php $twig = new Twig_Environment($loader); ?>
 
-<?php echo $twig->render('base.html', array('header' => 'Display Webcam Stream')); ?>
+<?php echo $twig->render('base.html', array('header' => 'Assignment-3')); ?>
 <?php require '../Assignment-3/upload.php';?>
 
 <!DOCTYPE html>
@@ -20,9 +20,72 @@
                 <input type="submit" value="Upload Image" name="submitbutton">
             </form>
 
-            <img width="50%" style="text-align:center;" src="./Assignment-3/image">
+            <img id="i" width="50%" style="text-align:center;" src="./Assignment-3/image">
         </div>
     </div>
+
+
+
+<script type="text/javascript">
+     var rgb = getAverageRGB(document.getElementById('i'));
+    document.body.style.backgroundColor = 'rgb('+rgb.r+','+rgb.b+','+rgb.g+')';
+
+function getAverageRGB(imgEl) {
+    
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+        
+    if (!context) {
+        return defaultRGB;
+    }
+    
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    
+    context.drawImage(imgEl, 0, 0);
+    
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */alert('x');
+        return defaultRGB;
+    }
+    
+    length = data.data.length;
+    
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+    
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+    
+    return rgb;
+    
+}
+
+
+
+
+
+    
+</script>
+
+
+
+
 
 
 </body>
