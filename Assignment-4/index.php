a8aa2c8170c69d6d1c52dd9b83e1cbffc9988f55
gigglesbw4 <blakewrege1@gmail.com>
2015-11-16 01:01:48 -0500

    changed get to post

--- Assignment-4/index.php
+++ Assignment-4/index.php
@@ -12,22 +12,20 @@
 
 <?php
  
-echo "<form action='./Assignment-4/index.php' method='get'>";
+echo "<form action='' method='post'>";
 echo "Number values to generate: <input type='text' name='N' />";
-echo "<input type='submit' />";
+echo "<input type='submit'  value='Submit Type' name='submitbutton'/>";
 echo "</form>";
  
-if(isset($_GET['N']))
-{
-  $N = $_GET['N'];
+if(isset($_POST['submitbutton'])){
+  $N = $_POST['N'];
  
   // execute R script from shell
   // this will save a plot at temp.png to the filesystem
   exec("Rscript my_rscript.R $N");
  
   // return image tag
-  $nocache = rand();
-  echo("<img src='./Assignment-4/temp.png?$nocache' />");
+  echo("<img src='./Assignment-4/temp.png' />");
 }
 ?>
 
