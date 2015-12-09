0d776d24eaa19a46c5b3d7625904059de27f8489
gigglesbw4 <blakewrege1@gmail.com>
2015-12-09 15:24:29 -0500

    fixed picture taking

--- Assignment-3/indexnew.php
+++ Assignment-3/indexnew.php
@@ -23,10 +23,9 @@
           <div class="row" style="text-align:center;">
             <form action="" method="post" enctype="multipart/form-data">
                 Select image to upload:
-                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
                 <input type="submit" value="Upload Image" name="submitbutton">
             </form>
-            <input type="file" id="take-picture" accept="image/*">
         </div>
 
 
